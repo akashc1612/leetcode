8670e65dfa2cd27b85002a8be67297192ca64219
akash singh chauhan <58021571+akashc1612@users.noreply.github.com>
2022-06-21 15:20:05 +0530

    Time: 330 ms (7.98%), Space: 5.9 MB (95.11%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s= "";
+        for(int i=0;i<n;i++){
+            s = s+to_string(i+1);
+        }
+        while(--k>0 && next_permutation(s.begin(),s.end()));
+        return s;
+    }
+};
