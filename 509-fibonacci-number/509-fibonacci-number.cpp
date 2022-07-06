7c20780011f2e3c0e415cb324c6a0bf8aef1f8c3
akash singh chauhan <58021571+akashc1612@users.noreply.github.com>
2022-07-06 11:00:58 +0530

    Time: 19 ms (22.24%), Space: 5.8 MB (96.23%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int f(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        else return f(n-1)+f(n-2);
+    }
+    int fib(int n) {
+        return f(n);
+    }
+};
