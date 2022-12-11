09f665ccd16cf09aac4ab67186521aaa79856d61
Akash Singh Chauhan <58021571+akashc1612@users.noreply.github.com>
2022-12-11 13:27:48 +0530

    Added solution - CodeHub

---
+++ Tour of King - codechef/tour-of-king.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<a*5+b*7<<endl;
+    }
+    return 0;
+}
