53bbd286c7242252ac4719906746cc4d7f43b96f
Akash Singh Chauhan <58021571+akashc1612@users.noreply.github.com>
2023-03-22 12:04:13 +0530

    Added solution - CodeHub

---
+++ Battery Health - codechef/battery-health.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        if(a>=80)cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
